16df4520cca718f1bf81a16bc6f78b409edd9fee
reimi <65539218+reimibeta@users.noreply.github.com>
2023-05-15 11:17:23 +0700

    .

--- server/index.js
+++ server/index.js
@@ -31,23 +31,23 @@ io.on("connection", (socket) => {
 // socket.on('disconnect', function(data){
 //     socket.broadcast.to(data.room).emit('user_leave', {user_name: "johnjoe123", sid: socket.id});
 // });
-const sales = io.of("/sales");
-sales.on("connection", (socket) => {
-    socket.on("join_room", (data) => {
-        socket.join(data);
-    });
-    // console.log('User Connected: ' + socket.id);
-    socket.on("send_message", (data) => {
-        // console.log(data);
-        // socket.broadcast.emit("receive_message", data);
-        socket.to(data.room).emit("receive_message", data);
-    });
-});
-sales.on('disconnect', function(){
-    // .to(data.room)
-    // socket.broadcast.emit('user_leave', {user: "johnjoe123", sid: socket.id});
-    console.log('user_leave', socket.id);
-});
+// const sales = io.of("/sales");
+// sales.on("connection", (socket) => {
+//     socket.on("join_room", (data) => {
+//         socket.join(data);
+//     });
+//     // console.log('User Connected: ' + socket.id);
+//     socket.on("send_message", (data) => {
+//         // console.log(data);
+//         // socket.broadcast.emit("receive_message", data);
+//         socket.to(data.room).emit("receive_message", data);
+//     });
+// });
+// sales.on('disconnect', function(){
+//     // .to(data.room)
+//     // socket.broadcast.emit('user_leave', {user: "johnjoe123", sid: socket.id});
+//     console.log('user_leave', socket.id);
+// });
 
 server.listen(3001, () => console.log('server running on port 3001'));
 
